7a8929512c4ed92e2e1f052d23017acdf98b9143
Hai Bui <haibui@pop-os.localdomain>
2020-06-28 10:52:56 -0700

    Add walking animation

--- src/impl/Entity/PlayerCharacters/Player/Player.cs
+++ src/impl/Entity/PlayerCharacters/Player/Player.cs
@@ -5,7 +5,7 @@ public class Player : GameObject
 	Weapon weapon;
 
 	public Player():
-		base(new PlayerInputController(), new PlayerPhysicsController(), new NullGraphicsController())
+		base(new PlayerInputController(), new PlayerPhysicsController(), new PlayerGraphicsController())
 	{
 		this.speed = 200;
 	}
@@ -13,16 +13,6 @@ public class Player : GameObject
 	public override void _Ready()
 	{
 		weapon = GetNode<Weapon>("Weapon");
-	}
-
-	public override void _Process(float delta)
-	{
-		inputController.Update(this);
-	}
-
-	public override void _PhysicsProcess(float delta)
-	{
-		physicsController.Update(this, delta);
 	}
 
 	public void Shoot(Vector2 vector)
---
+++ src/impl/Entity/PlayerCharacters/Player/PlayerGraphicsController.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public class PlayerGraphicsController : GraphicsController 
+{
+    public override void Update(GameObject gameObject)
+    {
+        Player player = (Player)gameObject;
+        AnimatedSprite animSprite = gameObject.GetNode<AnimatedSprite>("AnimatedSprite");
+        
+        bool goingLeft = player.velocity.x < 0;
+        animSprite.FlipH = goingLeft;
+
+        bool goingUp = player.velocity.y < 0;
+        if (goingUp) {
+            animSprite.Animation = "walkUp";
+        } else {
+            animSprite.Animation = "walk";
+        }
+
+        bool moving = player.velocity.Length() > 0.0001;
+        animSprite.Playing = moving;
+    }
+}
