80417dc0ddf4dd99e112fb408673aa53265ce693
bkrisztian01 <32880536+bkrisztian01@users.noreply.github.com>
2021-03-07 20:26:04 +0100

    Apply suggestions from code review
    Whitespace
    
    Co-authored-by: Duy Hai Bui <duyhai@users.noreply.github.com>

--- src/model/Entity/Bullet.cs
+++ src/model/Entity/Bullet.cs
@@ -2,34 +2,34 @@ using Godot;
 
 public abstract class Bullet : GameObject
 {
-	public int damage;
+    public int damage;
 
-	public Bullet(InputController inputController, PhysicsController physicsController, GraphicsController graphicsController, int damage) :
-		base(inputController, physicsController, graphicsController)
-	{
-		this.damage = damage;
-	}
+    public Bullet(InputController inputController, PhysicsController physicsController, GraphicsController graphicsController, int damage) :
+        base(inputController, physicsController, graphicsController)
+    {
+        this.damage = damage;
+    }
 
-	public void OnVisibilityNotifier2DScreenExited()
-	{
-		if (!GetTree().Paused)
-		{
-			QueueFree();
-		}
-	}
+    public void OnVisibilityNotifier2DScreenExited()
+    {
+        if (!GetTree().Paused)
+        {
+            QueueFree();
+        }
+    }
 
-	public void Initiate(float rotation, Vector2 position)
-	{
-		Position = position;
-		velocity = new Vector2(speed, 0).Rotated(rotation);
-	}
+    public void Initiate(float rotation, Vector2 position)
+    {
+        Position = position;
+        velocity = new Vector2(speed, 0).Rotated(rotation);
+    }
 
-	public virtual void HitTarget(KinematicCollision2D collision)
-	{
-		var collider = collision.Collider;
-		var method = collider.GetType().GetMethod("Hit");
-		method?.Invoke(collider, new object[] { damage });
-	}
+    public virtual void HitTarget(KinematicCollision2D collision)
+    {
+        var collider = collision.Collider;
+        var method = collider.GetType().GetMethod("Hit");
+        method?.Invoke(collider, new object[] { damage });
+    }
 
-	public override void Hit(int damage) { }
+    public override void Hit(int damage) { }
 }
