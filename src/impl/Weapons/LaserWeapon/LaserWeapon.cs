20463f83fb1dd342b611ecd0be2e688fd18288da
bkrisztian01 <32880536+bkrisztian01@users.noreply.github.com>
2021-07-19 20:54:18 +0200

    Laser weapon ray cast (#70)
    * update gitignore
    
    * LaserWeapon and RocketLauncher adjustment
    
    * Delete files

--- src/impl/Weapons/LaserWeapon/LaserWeapon.cs
+++ src/impl/Weapons/LaserWeapon/LaserWeapon.cs
@@ -3,86 +3,29 @@ using System;
 
 public class LaserWeapon : Weapon
 {
-    bool isCasting = false;
-    RayCast2D rayCast2D;
-    SoundPlayer soundPlayer;
+    public LaserWeapon() : base(new WeaponGraphicsController(), 3) { }
 
-    public LaserWeapon() : base(new LaserWeaponGraphicsController(), 3) { }
-
-    public override bool Shoot(Vector2 vector, uint collisionLayer, uint collisionMask)
-    {
-        GetNode<RayCast2D>("Tip/RayCast2D").CollisionMask = collisionMask;
-        setIsCasting(true);
-        bulletTimer.Start();
-        return true;
-    }
+    Timer triggerTimer;
+    LaserBullet laserBullet;
 
     public override void _Ready()
     {
-        // This BulletTimer is repurposed to help with 
-        // disabling the laser when the attack button is released.
-        // We can to this, because the laser doesn't have a cooldown.
-        bulletTimer = GetNode<Timer>("BulletTimer");
-        SetPhysicsProcess(false);
-        GetNode<Line2D>("Tip/RayCast2D/Line2D").Points[1] = Vector2.Zero;
-        rayCast2D = GetNode<RayCast2D>("Tip/RayCast2D");
+        triggerTimer = GetNode<Timer>("TriggerTimer");
+        laserBullet = GetNode<LaserBullet>("Tip/LaserBullet");
+        GameObject initiator = GetParent<GameObject>();
+        laserBullet.Initiate(initiator, 0, laserBullet.Position, damage);
+        bulletScene = LaserBullet.SceneObject;
     }
 
-    public override void _PhysicsProcess(float delta)
+    public override bool Shoot(Vector2 vector, uint collisionLayer, uint collisionMask)
     {
-        Vector2 castPoint = rayCast2D.CastTo;
-        rayCast2D.ForceRaycastUpdate();
-        var collisionParticles2D = GetNode<Particles2D>("Tip/RayCast2D/CollisionParticles2D");
-        collisionParticles2D.Emitting = rayCast2D.IsColliding();
-
-        if (rayCast2D.IsColliding())
-        {
-            castPoint = rayCast2D.ToLocal(rayCast2D.GetCollisionPoint());
-            var body = rayCast2D.GetCollider();
-            var method = body.GetType().GetMethod("Hit");
-            int inflictedDamage = (int)method?.Invoke(body, new object[] { damage });
-            if (inflictedDamage > 0)
-            {
-                GameObject initiator = GetParent<GameObject>();
-                float lifestealPercentage = 0f;
-                if (initiator.IsInsideTree())
-                {
-                    var powerUps = GroupUtils.FindNodeDescendantsInGroup(initiator, "LifestealPowerUp");
-                    for (int i = 0; i < powerUps.Count; i++)
-                    {
-                        LifestealPowerUp lifestealPowerUp = (LifestealPowerUp)powerUps[i];
-                        lifestealPercentage += lifestealPowerUp.Percentage;
-                    }
-                }
-                initiator.Hit((int)(-inflictedDamage * lifestealPercentage));
-            }
-            collisionParticles2D.GlobalRotation = rayCast2D.GetCollisionNormal().Angle();
-            collisionParticles2D.Position = castPoint;
-        }
-        GetNode<Line2D>("Tip/RayCast2D/Line2D").SetPointPosition(1, castPoint);
+        laserBullet.SetIsCasting(true);
+        triggerTimer.Start();
+        return true;
     }
 
-    void setIsCasting(bool cast)
+    public void OnTriggerTimerTimeout()
     {
-        if (cast && !isCasting)
-        {
-            ((LaserWeaponGraphicsController)graphicsController).appear(this);
-            soundPlayer = SoundManager.Instance.PlaySound(SoundPaths.LaserBeam, true);
-        }
-        else if (!cast && isCasting)
-        {
-            GetNode<Particles2D>("Tip/RayCast2D/CollisionParticles2D").Emitting = false;
-            ((LaserWeaponGraphicsController)graphicsController).disappear(this);
-            soundPlayer.QueueFree();
-        }
-
-        isCasting = cast;
-        SetPhysicsProcess(isCasting);
-        GetNode<Particles2D>("Tip/RayCast2D/CastingParticles2D").Emitting = cast;
-    }
-
-    public void OnBulletTimerTimeout()
-    {
-        setIsCasting(false);
+        laserBullet.SetIsCasting(false);
     }
 }
