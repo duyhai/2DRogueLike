93d71924fff860d28c97e0ca3c163622fff1d20c
bkrisztian01 <32880536+bkrisztian01@users.noreply.github.com>
2021-08-23 23:00:10 +0200

    Revert "Ball lightning weapon (#73)"
    This reverts commit a0b162f885886a6e5adfac270c1a55c062650161.

--- src/impl/Entity/PlayerCharacters/Player/Player.cs
+++ src/impl/Entity/PlayerCharacters/Player/Player.cs
@@ -26,7 +26,6 @@ public class Player : GameObject
         weapons.Add((Weapon)GetNode("ShockWeapon"));
         weapons.Add((Weapon)GetNode("Melee"));
         weapons.Add((Weapon)GetNode("LaserWeapon"));
-        weapons.Add((Weapon)GetNode("BallLightningWeapon"));
         weapons[equippedWeaponIndex].Visible = true;
     }
 
