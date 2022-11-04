0918d1830dba4e112555e58e007c9649cccdb59d
bkrisztian01 <bui.krisztian01@gmail.com>
2022-11-04 16:30:44 +0100

    CameraEffects

---
+++ src/impl/Entity/PlayerCharacters/Player/PlayerCameraController.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System.Collections.Generic;
+
+public class PlayerCameraController : CameraController
+{
+    List<CameraEffect> cameraEffects = new List<CameraEffect>();
+
+    public PlayerCameraController(Camera2D camera) : base(camera) { }
+
+    public override void Update(GameObject gameObject, float delta)
+    {
+        foreach (var effect in cameraEffects)
+        {
+            effect.Update(camera, delta);
+        }
+    }
+}
