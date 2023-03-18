c2610e008ca47bd13d9164862b7f168c5804cfe0
bkrisztian01 <bui.krisztian01@gmail.com>
2023-03-18 20:27:12 +0100

    Use tweener for floating text

--- src/impl/FloatingText.cs
+++ src/impl/FloatingText.cs
@@ -3,28 +3,16 @@ using System;
 
 public class FloatingText : Node2D
 {
-    Random rnd = new Random();
-    Vector2 movement;
-    Tween tween;
-    Label label;
-
     public void ShowValue(string value, Vector2 travel, float duration, int spread)
     {
+        Label label = GetNode<Label>("Label");
         label.Text = value;
+        Random rnd = new Random();
         float angle = 2 * 3.14f * rnd.Next(-spread / 2, spread / 2) / 360;
-        movement = travel.Rotated(angle);
-        tween.InterpolateProperty(this, "position", Position, Position + movement, duration, Tween.TransitionType.Linear, Tween.EaseType.InOut);
-        tween.Start();
-    }
-
-    public override void _Ready()
-    {
-        label = GetNode<Label>("Label");
-        tween = GetNode<Tween>("Tween");
-    }
-    
-    public void OnTweenAllCompleted()
-    {
-        QueueFree();
+        Vector2 movement = travel.Rotated(angle);
+        SceneTreeTween tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "position", Position + movement, duration).SetTrans(Tween.TransitionType.Linear).SetEase(Tween.EaseType.InOut);
+        tween.TweenCallback(this, "queue_free");
+        tween.Play();
     }
 }
