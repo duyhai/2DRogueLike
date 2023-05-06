4af0a7b4e8ade932ece1c80965322542dc3ef754
bkrisztian01 <bui.krisztian01@gmail.com>
2023-05-06 21:05:15 +0200

    Apply suggestion

--- src/impl/CameraEffects/ShakeCameraEffect.cs
+++ src/impl/CameraEffects/ShakeCameraEffect.cs
@@ -4,11 +4,11 @@ using System;
 public partial class ShakeCameraEffect : TimedCameraEffect
 {
     int Frequency { get; set; }
-    float Amplitude { get; set; }
-    float lastShookTimer = 0;
-    float periodInMs = 0;
-    float previousX = 0;
-    float previousY = 0;
+    double Amplitude { get; set; }
+    double lastShookTimer = 0;
+    double periodInMs = 0;
+    double previousX = 0;
+    double previousY = 0;
     Vector2 lastOffset = new Vector2(0, 0);
 
     public ShakeCameraEffect(float duration, int frequency, float amplitude) : base(duration)
@@ -32,26 +32,26 @@ public partial class ShakeCameraEffect : TimedCameraEffect
         while (lastShookTimer >= periodInMs)
         {
             lastShookTimer -= periodInMs;
-            float intensity = Amplitude * (1.0f - ((Duration - Timer) / Duration));
-            float newX = (float)(rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
-            float xComponent = intensity * (previousX + ((float)delta * (newX - previousX)));
-            float newY = (float)(rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
-            float yComponent = intensity * (previousY + ((float)delta * (newY - previousY)));
+            double intensity = Amplitude * (1.0f - ((Duration - Timer) / Duration));
+            double newX = (rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
+            double xComponent = intensity * (previousX + (delta * (newX - previousX)));
+            double newY = (rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
+            double yComponent = intensity * (previousY + (delta * (newY - previousY)));
             previousX = newX;
             previousY = newY;
-            Vector2 newOffset = new Vector2(xComponent, yComponent);
+            Vector2 newOffset = new Vector2((float)xComponent, (float)yComponent);
             camera.Offset = camera.Offset - lastOffset + newOffset;
             lastOffset = newOffset;
         }
 
-        lastShookTimer += (float)delta;
+        lastShookTimer += delta;
     }
 
     public override void Start(Camera2D camera)
     {
         base.Start(camera);
         Random rnd = new Random();
-        previousX = (float)(rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
-        previousY = (float)(rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
+        previousX = (rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
+        previousY = (rnd.NextDouble() * 2.0f) - 1.0f;  // -1.0 to 1.0
     }
 }
--- src/impl/Menu/MainMenu.cs
+++ src/impl/Menu/MainMenu.cs
@@ -2,16 +2,16 @@ using Godot;
 
 public partial class MainMenu : MarginContainer
 {
-	public override void _Ready()
-	{
-		base._Ready();
+    public override void _Ready()
+    {
+        base._Ready();
 
-		Input.MouseMode = Input.MouseModeEnum.Visible;
-	}
+        Input.MouseMode = Input.MouseModeEnum.Visible;
+    }
 
-	public override void _ExitTree()
-	{
-		Input.MouseMode = Input.MouseModeEnum.Hidden;
-		base._ExitTree();
-	}
+    public override void _ExitTree()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Hidden;
+        base._ExitTree();
+    }
 }
