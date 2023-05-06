5c56c61614d219dc049d2d919df5056080316a86
bkrisztian01 <bui.krisztian01@gmail.com>
2023-05-06 11:09:10 +0200

    Fixed snowblock sliding

--- src/impl/Map/SnowBlock.cs
+++ src/impl/Map/SnowBlock.cs
@@ -14,20 +14,20 @@ public partial class SnowBlock : Block
 
     public override void _Ready()
     {
-        CollisionLayer = CollisionLayers.Player | CollisionLayers.Enemy;
+        CollisionMask = CollisionLayers.Player | CollisionLayers.Enemy;
         area = GetNode<Area2D>("Area2D");
         area.CollisionLayer = CollisionLayer;
+        area.CollisionMask = CollisionMask;
     }
 
     public override void _Process(double delta)
     {
         foreach (var body in area.GetOverlappingBodies())
         {
-            if (body is GameObject)
+            if (body is GameObject gameObject)
             {
-                GameObject gameObject = (GameObject)body;
-                PowerUp powerUp = (PowerUp)slidingPowerUpScene.Instantiate();
-                powerUp.Initiate((GameObject)body, this);
+                SlidingPowerUp powerUp = (SlidingPowerUp)slidingPowerUpScene.Instantiate();
+                powerUp.Initiate(gameObject, this);
                 var method = body.GetType().GetMethod("AddPowerUp");
                 method?.Invoke(body, new object[] { powerUp });
             }
