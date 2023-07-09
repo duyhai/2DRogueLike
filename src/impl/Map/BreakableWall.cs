e21d0da8d2186b421fc93bb00a0848993de9f397
bkrisztian01 <bui.krisztian01@gmail.com>
2023-07-09 20:15:43 +0200

    Random breakable wall texture, crosshair

--- src/impl/Map/BreakableWall.cs
+++ src/impl/Map/BreakableWall.cs
@@ -6,7 +6,11 @@ public partial class BreakableWall : Block
     public BreakableWall() :
         base(new NullInputController(), new NullPhysicsController(), new NullGraphicsController())
     {
-
+        Textures = new Texture2D[] {
+            ResourceLoader.Load<Texture2D>("res://assets/blocks/breakablewall/breakablewall1.png"),
+            ResourceLoader.Load<Texture2D>("res://assets/blocks/breakablewall/breakablewall2.png"),
+            ResourceLoader.Load<Texture2D>("res://assets/blocks/breakablewall/breakablewall3.png"),
+        };
     }
 
     public override int Hit(int damage)
--- src/model/Map/Block.cs
+++ src/model/Map/Block.cs
@@ -1,15 +1,24 @@
+using System;
 using Godot;
 
 public abstract partial class Block : GameObject
 {
+    private Random rnd = new Random();
+    protected Texture2D[] Textures { get; set; }
+
     public Block(InputController inputController, PhysicsController physicsController, GraphicsController graphicsController) :
         base(inputController, physicsController, graphicsController)
-    {
-
-    }
+    { }
 
     public override void _Ready()
     {
+        Sprite2D sprite = GetNode<Sprite2D>("Sprite2D");
+        if (sprite != null && Textures != null && Textures.Length != 0)
+        {
+            sprite.Texture = Textures[rnd.Next(Textures.Length)];
+            sprite.FlipH = rnd.Next(2) == 1;
+        }
+
         CollisionLayer = CollisionLayers.Wall;
         CollisionMask = CollisionLayers.Player;
     }
