0d5309af2707e8be6351873f6ec3d51adf534a83
bkrisztian01 <bui.krisztian01@gmail.com>
2023-08-05 20:51:48 +0200

    Fix background, add inactive slot bg

--- src/impl/WeaponSlot.cs
+++ src/impl/WeaponSlot.cs
@@ -5,6 +5,7 @@ public partial class WeaponSlot : MarginContainer
 {
     public static PackedScene Scene = (PackedScene)GD.Load("res://scenes/hud/WeaponSlot.tscn");
     private TextureRect frameActive = null;
+    private TextureRect frameInactive = null;
 
     public Boolean Active
     {
@@ -13,6 +14,7 @@ public partial class WeaponSlot : MarginContainer
             if (frameActive != null)
             {
                 frameActive.Visible = value;
+                frameInactive.Visible = !value;
             }
         }
         get
@@ -32,6 +34,7 @@ public partial class WeaponSlot : MarginContainer
     public override void _Ready()
     {
         frameActive = GetNode<TextureRect>("FrameActive");
+        frameInactive = GetNode<TextureRect>("FrameInactive");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
